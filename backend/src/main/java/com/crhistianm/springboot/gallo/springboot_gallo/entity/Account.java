14a4a3de6921c9c9278534f7a9e52c73f624c1be
CrhistianMRe <came.29meca@gmail.com>
2025-07-05 21:04:52 +0800

    feat(backend): add hashCode/equals methods for all entities

--- backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/entity/Account.java
+++ backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/entity/Account.java
@@ -148,6 +148,37 @@ public class Account {
         return this;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Account other = (Account) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        return true;
+    }
+
     //person and roles here and as the main bidirectional relationship
     @Override
     public String toString() {
