dae76bfb8d96c413adf90cd3fb4ab466d5c52a53
CrhistianMRe <came.29meca@gmail.com>
2025-07-05 20:46:31 +0800

    test(backend): add custom inserts for h2 db

---
+++ backend/src/test/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO person (first_name, last_name, phone_number, birth_date, gender, height, weight) VALUES
+('Crhistian', 'Mendez', '111222333', '2000-01-01', 'M', 1.74, 80),
+('Erick', 'Perez', '222333444', '2004-01-01', 'M', 1.90, 60),
+('Daniela', 'Sanchez', '4443555666', '2009-01-01', 'F', 1.50, 50);
+
+
