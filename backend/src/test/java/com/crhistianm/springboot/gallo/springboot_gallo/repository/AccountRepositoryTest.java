3dfb6aa79721c276f0b5185eed9201c8c16dea21
CrhistianMRe <came.29meca@gmail.com>
2025-07-07 15:12:35 +0800

    test(backend): add integration tests for AccountRepository

---
+++ backend/src/test/java/com/crhistianm/springboot/gallo/springboot_gallo/repository/AccountRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.crhistianm.springboot.gallo.springboot_gallo.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.jdbc.Sql;
+
+import com.crhistianm.springboot.gallo.springboot_gallo.data.Data;
+import com.crhistianm.springboot.gallo.springboot_gallo.entity.Account;
+
+@DataJpaTest
+@Sql("/personinserts.sql")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
+public class AccountRepositoryTest {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Test
+    void testSave(){
+        //With id 1 person
+        Account account = new Account("example@gmail.com", "12345", Data.createPerson().orElseThrow());
+        accountRepository.save(account);
+
+        assertTrue(accountRepository.findByEmail("example@gmail.com").isPresent());
+        assertEquals("example@gmail.com",accountRepository.findByEmail("example@gmail.com").orElseThrow().getEmail());
+    }
+}
