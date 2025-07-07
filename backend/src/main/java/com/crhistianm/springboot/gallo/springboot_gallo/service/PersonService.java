4fb695eca32e7205fbc712bb73e4bae9412056a7
CrhistianMRe <came.29meca@gmail.com>
2025-07-07 14:49:41 +0800

    feat(backend): add register functionality for Person entity

---
+++ backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/service/PersonService.java
@@ -0,0 +1,10 @@
+package com.crhistianm.springboot.gallo.springboot_gallo.service;
+
+import com.crhistianm.springboot.gallo.springboot_gallo.entity.Person;
+
+public interface PersonService {
+   
+    Person save(Person Person);
+
+}
+
