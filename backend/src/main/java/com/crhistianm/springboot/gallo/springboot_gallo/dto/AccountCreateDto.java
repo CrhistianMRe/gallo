1d2e721d40870031eb4a3e14246266fcfece4993
CrhistianMRe <came.29meca@gmail.com>
2025-07-15 21:24:16 +0800

    feat(backend): add dto classes for Account register module to separate role tasks

--- backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/dto/AccountCreateDto.java
+++ backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/dto/AccountCreateDto.java
@@ -1,9 +1,7 @@
 package com.crhistianm.springboot.gallo.springboot_gallo.dto;
 
-//Dto for just receiving person id fk
-public class AccountDto {
-
-    private Long id;
+//Id not in dto as it is auto incremental on db
+public class AccountCreateDto{
 
     private Long personId;
 
@@ -13,10 +11,10 @@ public class AccountDto {
 
     private boolean admin;
 
-    public AccountDto() {
+    public AccountCreateDto() {
     }
 
-    public AccountDto(String email, String password, Long personId, boolean admin) {
+    public AccountCreateDto(String email, String password, Long personId, boolean admin) {
         this.email = email;
         this.password = password;
         this.admin = admin;
@@ -29,14 +27,6 @@ public class AccountDto {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public Long getPersonId() {
@@ -67,7 +57,7 @@ public class AccountDto {
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((personId == null) ? 0 : personId.hashCode());
         result = prime * result + ((email == null) ? 0 : email.hashCode());
         return result;
     }
@@ -80,11 +70,11 @@ public class AccountDto {
             return false;
         if (getClass() != obj.getClass())
             return false;
-        AccountDto other = (AccountDto) obj;
-        if (id == null) {
-            if (other.id != null)
+        AccountCreateDto other = (AccountCreateDto) obj;
+        if (personId == null) {
+            if (other.personId != null)
                 return false;
-        } else if (!id.equals(other.id))
+        } else if (!personId.equals(other.personId))
             return false;
         if (email == null) {
             if (other.email != null)
@@ -96,7 +86,8 @@ public class AccountDto {
 
     @Override
     public String toString() {
-        return "AccountDto [id=" + id + ", personId=" + personId + "]";
+        return "AccountCreateDto [personId=" + personId + ", email=" + email + ", password=" + password + ", admin="
+                + admin + "]";
     }
 
 }
---
+++ backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/dto/AccountResponseDto.java
@@ -0,0 +1,10 @@
+package com.crhistianm.springboot.gallo.springboot_gallo.dto;
+
+
+public interface AccountResponseDto {
+
+    String getEmail();
+
+    void setEmail(String email);
+    
+}
