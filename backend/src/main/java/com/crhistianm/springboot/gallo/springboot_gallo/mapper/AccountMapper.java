d37e36051d0004e88bf86d3f3bda3ff62b7b89eb
CrhistianMRe <came.29meca@gmail.com>
2025-07-15 21:25:27 +0800

    feat(backend): add mapper for Account register module

---
+++ backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/mapper/AccountMapper.java
@@ -0,0 +1,30 @@
+package com.crhistianm.springboot.gallo.springboot_gallo.mapper;
+
+import com.crhistianm.springboot.gallo.springboot_gallo.builder.AccountBuilder;
+import com.crhistianm.springboot.gallo.springboot_gallo.dto.AccountAdminResponseDto;
+import com.crhistianm.springboot.gallo.springboot_gallo.dto.AccountCreateDto;
+import com.crhistianm.springboot.gallo.springboot_gallo.dto.AccountResponseDto;
+import com.crhistianm.springboot.gallo.springboot_gallo.dto.AccountUserResponseDto;
+import com.crhistianm.springboot.gallo.springboot_gallo.entity.Account;
+
+public class AccountMapper {
+
+    public static Account createToEntity(AccountCreateDto accountDto){
+        return new AccountBuilder().email(accountDto.getEmail()).password(accountDto.getPassword()).build();
+    }
+
+    public static AccountResponseDto entityToAdminResponse(Account account){
+        AccountAdminResponseDto accountDto = new AccountAdminResponseDto();
+        accountDto.setEmail(account.getEmail());
+        accountDto.setRoles(account.getRoles());
+        accountDto.setPerson(account.getPerson());
+        accountDto.setAudit(account.getAudit());
+        return accountDto;
+    }
+    
+    public static AccountResponseDto entityToResponse(Account account){
+        AccountUserResponseDto accountDto = new AccountUserResponseDto();
+        accountDto.setEmail(account.getEmail());
+        return accountDto;
+    }
+}
