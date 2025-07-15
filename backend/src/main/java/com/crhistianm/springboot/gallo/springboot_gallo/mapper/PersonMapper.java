c4365319a364fc1d6b8acbd8d090a62ba992ad5e
CrhistianMRe <came.29meca@gmail.com>
2025-07-15 21:15:23 +0800

    feat(backend): add mapper for Person register module classes

---
+++ backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/mapper/PersonMapper.java
@@ -0,0 +1,35 @@
+package com.crhistianm.springboot.gallo.springboot_gallo.mapper;
+
+import com.crhistianm.springboot.gallo.springboot_gallo.builder.PersonBuilder;
+import com.crhistianm.springboot.gallo.springboot_gallo.dto.PersonCreateDto;
+import com.crhistianm.springboot.gallo.springboot_gallo.dto.PersonResponseDto;
+import com.crhistianm.springboot.gallo.springboot_gallo.entity.Person;
+
+public class PersonMapper {
+
+    public static Person createToEntity(PersonCreateDto dto){
+        return new PersonBuilder().firstName(dto.getFirstName())
+            .lastName(dto.getLastName())
+            .phoneNumber(dto.getPhoneNumber())
+            .birthDate(dto.getBirthDate())
+            .gender(dto.getGender())
+            .height(dto.getHeight())
+            .weight(dto.getWeight())
+            .build();
+    }
+
+
+    public static PersonResponseDto entityToResponse(Person person){
+        PersonResponseDto dto = new PersonResponseDto();
+        dto.setId(person.getId());
+        dto.setFirstName(person.getFirstName());
+        dto.setLastName(person.getLastName());
+        dto.setPhoneNumber(person.getPhoneNumber());
+        dto.setBirthDate(person.getBirthDate());
+        dto.setGender(person.getGender());
+        dto.setHeight(person.getHeight());
+        dto.setWeight(person.getWeight());
+        dto.setAccount(person.getAccount());
+        return dto;
+    }
+}
