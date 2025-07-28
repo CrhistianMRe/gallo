9cc0fc7a0805c9be212152584c3daf8ba2993d38
CrhistianMRe <came.29meca@gmail.com>
2025-07-28 13:19:17 +0800

    test(backend): add Annotation integration tests for each annotated validated field on AccountCreateDto

---
+++ backend/src/test/java/com/crhistianm/springboot/gallo/springboot_gallo/validation/dto/AccountCreateDtoAnnotationTest.java
@@ -0,0 +1,195 @@
+package com.crhistianm.springboot.gallo.springboot_gallo.validation.dto;
+
+import static com.crhistianm.springboot.gallo.springboot_gallo.data.Data.createAccountDto;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+
+import com.crhistianm.springboot.gallo.springboot_gallo.dto.AccountCreateDto;
+import com.crhistianm.springboot.gallo.springboot_gallo.service.AccountServiceImpl;
+import com.crhistianm.springboot.gallo.springboot_gallo.service.PersonServiceImpl;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
+
+@SpringBootTest
+public class AccountCreateDtoAnnotationTest{
+
+    @Autowired
+    private Validator validator;
+
+    private AccountCreateDto account;
+
+    @MockitoBean
+    private PersonServiceImpl personService;
+
+    @MockitoBean
+    private AccountServiceImpl accountService;
+
+    Set<ConstraintViolation<AccountCreateDto>> violations;
+
+    @BeforeEach
+    void setUp(){
+            //To avoid message general validations until is needed
+            lenient().when(personService.isPersonRegistered(anyLong())).thenReturn(true);
+            lenient().when(personService.isPhoneNumberAvailable(anyString())).thenReturn(true);
+            lenient().when(accountService.isEmailAvailable(anyString())).thenReturn(true);
+    }
+
+    @Nested
+    class PersonIdFieldTest{
+
+        @BeforeEach
+        void setUp(){
+            account = createAccountDto().orElseThrow();
+        }
+
+        @Test
+        void testInvalidNotNull(){
+            account.setPersonId(null);
+            violations = validator.validate(account);
+            assertFalse(violations.isEmpty());
+            assertThat(violations).hasSize(1);
+            assertThat(violations).extracting(ConstraintViolation::getMessage).containsOnly("must not be null");
+        }
+        
+        @Test 
+        void testValidNotNull(){
+            violations = validator.validate(account);
+            assertTrue(violations.isEmpty());
+            assertThat(violations).hasSize(0);
+        }
+
+        @Test
+        void testInvalidPersonNotAssigned(){
+            when(accountService.isPersonIdAssigned(anyLong())).thenReturn(true);
+            violations = validator.validate(account);
+            assertFalse(violations.isEmpty());
+            assertThat(violations).hasSize(1);
+            assertThat(violations).extracting(ConstraintViolation::getMessage).containsOnly("is already assigned to an account, please use another one");
+        }
+
+        @Test
+        void testValidPersonNotAssigned(){
+            when(accountService.isPersonIdAssigned(anyLong())).thenReturn(false);
+            violations = validator.validate(account);
+            assertTrue(violations.isEmpty());
+            assertThat(violations).hasSize(0);
+        }
+
+        @Test
+        void testInvalidPersonRegistered(){
+            when(personService.isPersonRegistered(anyLong())).thenReturn(false);
+            violations = validator.validate(account);
+            assertFalse(violations.isEmpty());
+            assertThat(violations).hasSize(1);
+            assertThat(violations).extracting(ConstraintViolation::getMessage).containsOnly("is not registered yet, register person first");
+        }
+        
+        @Test
+        void testValidPersonRegistered(){
+            when(personService.isPersonRegistered(anyLong())).thenReturn(true);
+            violations = validator.validate(account);
+            assertTrue(violations.isEmpty());
+            assertThat(violations).hasSize(0);
+        }
+    }
+
+    @Nested
+    class emailFieldTest{
+        
+        @BeforeEach
+        void setUp(){
+            account = createAccountDto().orElseThrow();
+        }
+
+        @Test
+        void testInvalidNotBlank(){
+            account.setEmail("");
+            violations = validator.validate(account);
+            assertFalse(violations.isEmpty());
+            assertThat(violations).hasSize(1);
+            assertThat(violations).extracting(ConstraintViolation::getMessage).containsOnly("must not be blank");
+        }
+
+        @Test
+        void testValidNotBlank(){
+            violations = validator.validate(account);
+            assertTrue(violations.isEmpty());
+            assertThat(violations).hasSize(0);
+        }
+
+        @Test
+        void testInvalidUniqueEmail(){
+            when(accountService.isEmailAvailable(anyString())).thenReturn(false);
+            violations = validator.validate(account);
+            assertFalse(violations.isEmpty());
+            assertThat(violations).hasSize(1);
+            assertThat(violations).extracting(ConstraintViolation::getMessage).containsOnly("is already registered, user another one!");
+        }
+
+        @Test
+        void testValidUniqueEmail(){
+            violations = validator.validate(account);
+            assertTrue(violations.isEmpty());
+            assertThat(violations).hasSize(0);
+        }
+
+        @Test
+        void testInvalidEmail(){
+            account.setEmail("notavalidemail,com");
+            violations = validator.validate(account);
+            assertFalse(violations.isEmpty());
+            assertThat(violations).hasSize(1);
+            assertThat(violations).extracting(ConstraintViolation::getMessage).containsOnly("must be a well-formed email address");
+        }
+
+        @Test
+        void testValidEmail(){
+            violations = validator.validate(account);
+            assertTrue(violations.isEmpty());
+            assertThat(violations).hasSize(0);
+        }
+
+    }
+
+    @Nested
+    class passwordFieldTest{
+
+        @BeforeEach
+        void setUp(){
+            account = createAccountDto().orElseThrow();
+        }
+
+        @Test
+        void testInvalidNotBlank(){
+            account.setPassword("");
+            violations = validator.validate(account);
+            assertFalse(violations.isEmpty());
+            assertThat(violations).hasSize(1);
+            assertThat(violations).extracting(ConstraintViolation::getMessage).containsOnly("must not be blank");
+        }
+
+        @Test
+        void testValidNotBlank(){
+            violations = validator.validate(account);
+            assertTrue(violations.isEmpty());
+            assertThat(violations).hasSize(0);
+        }
+
+    }
+
+
+    
+}
