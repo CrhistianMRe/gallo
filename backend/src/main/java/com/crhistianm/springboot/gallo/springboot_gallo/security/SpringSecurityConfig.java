7948b5bc526c8be7aadb1135d27dc51157a71cdd
CrhistianMRe <came.29meca@gmail.com>
2025-07-29 22:44:05 +0800

    feat(backend): set up AuthenticationManager bean to enable AccountUserDetailService

--- backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/security/SpringSecurityConfig.java
+++ backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/security/SpringSecurityConfig.java
@@ -3,6 +3,8 @@ package com.crhistianm.springboot.gallo.springboot_gallo.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -13,6 +15,17 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableMethodSecurity(prePostEnabled = true)
 public class SpringSecurityConfig {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    public SpringSecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+        this.authenticationConfiguration = authenticationConfiguration;
+    } 
+
+    @Bean
+    AuthenticationManager authenticationManager() throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Bean
     PasswordEncoder passwordEncoder(){
