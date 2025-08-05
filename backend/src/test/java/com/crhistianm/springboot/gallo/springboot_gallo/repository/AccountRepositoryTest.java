0cebcffc91f13a35dcb8e0093b9ca83609121b2d
CrhistianMRe <came.29meca@gmail.com>
2025-08-05 17:37:58 +0800

    test(backend): add integration db tests for personalized queries of AccountRepository

--- backend/src/test/java/com/crhistianm/springboot/gallo/springboot_gallo/repository/AccountRepositoryTest.java
+++ backend/src/test/java/com/crhistianm/springboot/gallo/springboot_gallo/repository/AccountRepositoryTest.java
@@ -1,7 +1,14 @@
 package com.crhistianm.springboot.gallo.springboot_gallo.repository;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static com.crhistianm.springboot.gallo.springboot_gallo.data.Data.createAdminRole;
+import static com.crhistianm.springboot.gallo.springboot_gallo.data.Data.createUserRole;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,14 +21,14 @@ import com.crhistianm.springboot.gallo.springboot_gallo.builder.PersonBuilder;
 import com.crhistianm.springboot.gallo.springboot_gallo.entity.Account;
 
 @DataJpaTest
-@Sql("/personinserts.sql")
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class AccountRepositoryTest {
 
     @Autowired
     private AccountRepository accountRepository;
 
     @Test
+    @Sql("/personinserts.sql")
     void testSave(){
         Account account = new AccountBuilder().email("example@gmail.com").person(new PersonBuilder().firstName("two").id(1L).build()).build();
         accountRepository.save(account);
@@ -29,4 +36,40 @@ public class AccountRepositoryTest {
         assertTrue(accountRepository.findByEmail("example@gmail.com").isPresent());
         assertEquals("example@gmail.com", accountRepository.findByEmail("example@gmail.com").orElseThrow().getEmail());
     }
+
+    @Test
+    @Sql({"/personinserts.sql", "/accountinserts.sql"})
+    void testFindAccountByPersonId(){
+        Account account = new AccountBuilder()
+            .id(1L)
+            .email("crhistian@gmail.com")
+            .build();
+
+
+        Optional<Account> accountOptional = accountRepository.findAccountByPersonId(1L);
+
+        assertTrue(accountOptional.isPresent());
+        assertEquals(account, accountOptional.orElseThrow());
+    }
+
+    @Test
+    @Sql({"/personinserts.sql", "/roleinserts.sql", "/accountinserts.sql", "/accountroleinserts.sql"})
+    void testFindByEmailWithRoles(){
+        Account account = new AccountBuilder()
+            .id(1L)
+            .email("crhistian@gmail.com")
+            .person(new PersonBuilder()
+                    .firstName("Crhistian").lastName("Mendez").phoneNumber("111222333").birthDate(LocalDate.of(2000, 01, 01)).weight(80.0).gender("M").height(1.74).build())
+            .roles(List.of(createAdminRole().orElseThrow(), createUserRole().orElseThrow()))
+            .build();
+
+        Optional<Account> accountOptional = accountRepository.findByEmailWithRoles("crhistian@gmail.com");
+
+        assertTrue(accountOptional.isPresent());
+        assertThat(accountOptional.orElseThrow().getRoles()).hasSize(2);
+
+        assertEquals(account.getEmail(), accountOptional.orElseThrow().getEmail());
+        assertEquals(account, accountOptional.orElseThrow());
+    }
+
 }
---
+++ backend/src/test/resources/accountinserts.sql
@@ -0,0 +1,5 @@
+INSERT INTO account (email, password, created_at, enabled, person_id) VALUES 
+( 'crhistian@gmail.com', 12345, '2025-01-01 18:13:54', 1, 1),
+( 'erick@gmail.com', 12345, '2025-02-02 18:13:54', 1, 2);
+
+
