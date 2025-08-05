c3204d3bac3c73bab7237acc7054211fe3dd95a1
CrhistianMRe <came.29meca@gmail.com>
2025-08-05 17:35:53 +0800

    test(backend): add account entity complete sample data for tests

--- backend/src/test/java/com/crhistianm/springboot/gallo/springboot_gallo/data/Data.java
+++ backend/src/test/java/com/crhistianm/springboot/gallo/springboot_gallo/data/Data.java
@@ -2,12 +2,22 @@ package com.crhistianm.springboot.gallo.springboot_gallo.data;
 
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
+import org.assertj.core.util.Arrays;
+
+import com.crhistianm.springboot.gallo.springboot_gallo.builder.AccountBuilder;
 import com.crhistianm.springboot.gallo.springboot_gallo.builder.RoleBuilder;
+import com.crhistianm.springboot.gallo.springboot_gallo.dto.AccountAdminResponseDto;
 import com.crhistianm.springboot.gallo.springboot_gallo.dto.AccountCreateDto;
 import com.crhistianm.springboot.gallo.springboot_gallo.dto.PersonCreateDto;
+import com.crhistianm.springboot.gallo.springboot_gallo.entity.Account;
+import com.crhistianm.springboot.gallo.springboot_gallo.entity.Audit;
+import com.crhistianm.springboot.gallo.springboot_gallo.entity.Person;
 import com.crhistianm.springboot.gallo.springboot_gallo.entity.Role;
+import com.crhistianm.springboot.gallo.springboot_gallo.mapper.PersonMapper;
 
 public class Data {
 
@@ -54,6 +64,42 @@ public class Data {
 
     public static Optional<Role> createUserRole(){
         return Optional.of(new RoleBuilder().name("ROLE_USER").build());
+    }
+
+    public static Optional<Account> createAccountAdmin(){
+        Audit audit = new Audit();
+        audit.setEnabled(true);
+        audit.setCreatedAt(LocalDateTime.of(2004, 01, 01, 1, 1, 1, 1));
+        audit.setUpdatedAt(LocalDateTime.of(2004, 02, 01, 1, 1, 1, 1));
+        
+        return Optional.of(new AccountBuilder()
+                .id(1L)
+                .email("admin@gmail.com")
+                .password("12345")
+                .person(PersonMapper.createToEntity(createPersonOneDto().orElseThrow()))
+                .roles(List.of(createAdminRole().orElseThrow(), createUserRole().orElseThrow()))
+                .audit(audit)
+                .workouts(null)
+                .build()
+                );
+    }
+
+    public static Optional<Account> createAccountUser(){
+        Audit audit = new Audit();
+        audit.setEnabled(true);
+        audit.setCreatedAt(LocalDateTime.of(2004, 01, 01, 1, 1, 1, 1));
+        audit.setUpdatedAt(LocalDateTime.of(2004, 02, 01, 1, 1, 1, 1));
+        
+        return Optional.of(new AccountBuilder()
+                .id(1L)
+                .email("user@gmail.com")
+                .password("12345")
+                .person(PersonMapper.createToEntity(createPersonOneDto().orElseThrow()))
+                .roles(List.of(createUserRole().orElseThrow()))
+                .audit(audit)
+                .workouts(null)
+                .build()
+                );
     }
 
 }
