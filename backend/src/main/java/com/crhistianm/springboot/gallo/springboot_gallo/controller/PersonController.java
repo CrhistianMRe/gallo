f3800ae1a95995ba54a4a95b32a4459518a624a0
CrhistianMRe <came.29meca@gmail.com>
2025-08-13 12:44:48 +0800

    feat(backend): add person update functionality

--- backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/controller/PersonController.java
+++ backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/controller/PersonController.java
@@ -1,16 +1,20 @@
 package com.crhistianm.springboot.gallo.springboot_gallo.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.crhistianm.springboot.gallo.springboot_gallo.dto.PersonRequestDto;
+import com.crhistianm.springboot.gallo.springboot_gallo.dto.PersonResponseDto;
 import com.crhistianm.springboot.gallo.springboot_gallo.service.PersonService;
 
 import jakarta.validation.Valid;
@@ -36,5 +40,14 @@ public class PersonController {
     @GetMapping("/{id}")
     public ResponseEntity<?> viewById(@PathVariable Long id){
         return ResponseEntity.ok(personService.getById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@Valid @RequestBody PersonRequestDto person, @PathVariable Long id){
+
+        Optional<PersonResponseDto> responseDto = personService.update(id, person);
+
+        if (responseDto.isPresent()) return ResponseEntity.ok(responseDto.orElseThrow());
+        return ResponseEntity.notFound().build();
     }
 }
--- backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/service/PersonService.java
+++ backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/service/PersonService.java
@@ -1,6 +1,7 @@
 package com.crhistianm.springboot.gallo.springboot_gallo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.crhistianm.springboot.gallo.springboot_gallo.dto.PersonRequestDto;
 import com.crhistianm.springboot.gallo.springboot_gallo.dto.PersonResponseDto;
@@ -8,6 +9,8 @@ import com.crhistianm.springboot.gallo.springboot_gallo.dto.PersonResponseDto;
 public interface PersonService {
    
     PersonResponseDto save(PersonRequestDto createDto);
+
+    Optional<PersonResponseDto> update(Long id, PersonRequestDto personDto);
 
     boolean isPhoneNumberAvailable(String phoneNumber);
 
