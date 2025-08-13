72f3c5d8c923894d235a4cef6cb1982beb430c27
CrhistianMRe <came.29meca@gmail.com>
2025-08-13 12:45:27 +0800

    feat(backend): expose update functionality endpoint to admin authority

--- backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/security/SpringSecurityConfig.java
+++ backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/security/SpringSecurityConfig.java
@@ -41,6 +41,7 @@ public class SpringSecurityConfig {
         return http.authorizeHttpRequests((authz) -> authz 
                 .requestMatchers(HttpMethod.GET, "/api/persons").hasRole("ADMIN")
                 .requestMatchers(HttpMethod.GET, "/api/persons/{id}").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/api/persons/{id}").hasRole("ADMIN")
                     .requestMatchers(HttpMethod.POST, "/api/persons/register").hasAnyRole("ADMIN", "USER")
                     .requestMatchers(HttpMethod.POST, "/api/accounts/register").hasAnyRole("ADMIN", "USER")
                     .requestMatchers("/swagger-ui/**").hasRole("ADMIN")
