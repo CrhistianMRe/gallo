431b60bdfdd6cf02735d75f64fc21434e80e198a
CrhistianMRe <came.29meca@gmail.com>
2025-08-21 21:58:42 +0800

    feat(backend): add custom NotFoundException class handling

--- backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/controller/HandlerExceptionController.java
+++ backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/controller/HandlerExceptionController.java
@@ -1,13 +1,20 @@
 package com.crhistianm.springboot.gallo.springboot_gallo.controller;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeParseException;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.crhistianm.springboot.gallo.springboot_gallo.exception.NotFoundException;
 
 @RestControllerAdvice
 public class HandlerExceptionController {
@@ -28,6 +35,16 @@ public class HandlerExceptionController {
         Map<String, String> errors = new HashMap<>();
         errors.put(ex.getParsedString(), "not valid, use yyyy/mm/dd");
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, Object> handleNotFoundException(NotFoundException ex){
+        Map<String, Object> errors = new HashMap<>();
+        errors.put("date", new Date().toString());
+        errors.put("message", ex.getMessage());
+        errors.put("status", HttpStatus.NOT_FOUND.value());
+        return errors;
     }
     
 }
