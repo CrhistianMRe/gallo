7fabf13eda12294f44dbaa5ece643fc4b74f4e74
CrhistianMRe <came.29meca@gmail.com>
2025-09-03 21:25:39 +0800

    feat(backend): add account view by id endpoint functionality

--- backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/controller/AccountController.java
+++ backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/controller/AccountController.java
@@ -3,12 +3,15 @@ package com.crhistianm.springboot.gallo.springboot_gallo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.crhistianm.springboot.gallo.springboot_gallo.dto.AccountRequestDto;
+import com.crhistianm.springboot.gallo.springboot_gallo.dto.AccountResponseDto;
 import com.crhistianm.springboot.gallo.springboot_gallo.service.AccountService;
 
 import jakarta.validation.Valid;
@@ -23,6 +26,11 @@ public class AccountController {
     @PostMapping("/register")
     public ResponseEntity<?> create(@Valid @RequestBody AccountRequestDto accountDto){
         return ResponseEntity.status(HttpStatus.CREATED).body(accountService.save(accountDto));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AccountResponseDto> viewById(@PathVariable Long id){
+        return ResponseEntity.ok(accountService.getById(id));
     }
 
 
