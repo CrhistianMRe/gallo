0fa1cfa57a7448258a7fc03118183270c1a88d45
CrhistianMRe <106357444+CrhistianMRe@users.noreply.github.com>
2025-10-30 16:06:53 +0800

    Revert "feat(backend): add missing account self data access validation for view by id functionality"
    This reverts commit 0d3775c7150b4eb31e241535afbdc77644c9cc98.

--- backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/service/AccountServiceImpl.java
+++ backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/service/AccountServiceImpl.java
@@ -19,7 +19,6 @@ import com.crhistianm.springboot.gallo.springboot_gallo.entity.Account;
 import com.crhistianm.springboot.gallo.springboot_gallo.entity.Person;
 import com.crhistianm.springboot.gallo.springboot_gallo.entity.Role;
 import com.crhistianm.springboot.gallo.springboot_gallo.exception.NotFoundException;
-import com.crhistianm.springboot.gallo.springboot_gallo.exception.ValidationServiceException;
 import com.crhistianm.springboot.gallo.springboot_gallo.mapper.AccountMapper;
 import com.crhistianm.springboot.gallo.springboot_gallo.mapper.RoleMapper;
 import com.crhistianm.springboot.gallo.springboot_gallo.repository.AccountRepository;
@@ -42,16 +41,13 @@ public class AccountServiceImpl implements AccountService{
 
     private final AccountValidationService validationService;
 
-    private final IdentityVerificationService identityService;
-
-    public AccountServiceImpl(AccountRepository accountRepository, PersonRepository personRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder, AccountValidator accountValidator, AccountValidationService validationService, IdentityVerificationService identityService){
+    public AccountServiceImpl(AccountRepository accountRepository, PersonRepository personRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder, AccountValidator accountValidator, AccountValidationService validationService){
         this.accountRepository = accountRepository;
         this.personRepository = personRepository;
         this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
         this.accountValidator = accountValidator;
         this.validationService = validationService;
-        this.identityService = identityService;
     }
 
     @Transactional
@@ -100,9 +96,6 @@ public class AccountServiceImpl implements AccountService{
     @Override
     public AccountResponseDto getById(Long id) {
         Account account = accountRepository.findById(id).orElseThrow(() -> new NotFoundException(Account.class));
-        identityService.validateUserAllowance(account.getPerson().getId()).ifPresent(f ->{
-            throw new ValidationServiceException(new ArrayList<>(List.of(f)));
-        });
         return validationService.settleResponseType(account);
     }
 
