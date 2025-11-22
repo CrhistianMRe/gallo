826d6be7dcdd4b130391e4d53f092a6d9151a8fb
CrhistianMRe <came.29meca@gmail.com>
2025-11-22 19:45:14 +0800

    test(backend): rework account controller register module tests to add DTO scenario

---
+++ backend/src/test/java/com/crhistianm/springboot/gallo/springboot_gallo/config/ControllerValidatorConfig.java
@@ -0,0 +1,15 @@
+package com.crhistianm.springboot.gallo.springboot_gallo.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@TestConfiguration
+public class ControllerValidatorConfig {
+
+    @Bean
+    LocalValidatorFactoryBean localValidatorFactoryBean() {
+        return new LocalValidatorFactoryBean();
+    }
+    
+}
