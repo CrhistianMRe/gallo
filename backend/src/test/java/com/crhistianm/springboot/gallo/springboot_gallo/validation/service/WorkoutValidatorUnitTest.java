c406677353832c61e508da6fd2b563d31bbe6aad
CrhistianMRe <came.29meca@gmail.com>
2025-12-17 15:30:11 +0800

    test(backend): fix broken tests for Workout Validator rework

--- backend/src/test/java/com/crhistianm/springboot/gallo/springboot_gallo/validation/service/WorkoutValidatorUnitTest.java
+++ backend/src/test/java/com/crhistianm/springboot/gallo/springboot_gallo/validation/service/WorkoutValidatorUnitTest.java
@@ -25,7 +25,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.crhistianm.springboot.gallo.springboot_gallo.builder.FieldInfoErrorBuilder;
 import com.crhistianm.springboot.gallo.springboot_gallo.entity.Account;
-import com.crhistianm.springboot.gallo.springboot_gallo.exception.NotFoundException;
 import com.crhistianm.springboot.gallo.springboot_gallo.exception.ValidationServiceException;
 import com.crhistianm.springboot.gallo.springboot_gallo.model.FieldInfoError;
 import com.crhistianm.springboot.gallo.springboot_gallo.repository.AccountRepository;
@@ -33,9 +32,6 @@ import com.crhistianm.springboot.gallo.springboot_gallo.service.IdentityVerifica
 
 @ExtendWith(MockitoExtension.class)
 public class WorkoutValidatorUnitTest {
-
-    @Mock
-    private AccountRepository accountRepository;
 
     @Mock
     private IdentityVerificationService identityVerificationService;
@@ -51,44 +47,15 @@ public class WorkoutValidatorUnitTest {
         @BeforeEach
         void setUp() {
             accountId = null;
-
             doAnswer(invo -> {
-                Account account = null;
-                Long argId = invo.getArgument(0, Long.class);
-                if(argId.equals(1L)) {
-                    account = givenAccountEntityAdmin().orElseThrow();
-                    account.getPerson().setId(1L);
-                }
-                if(argId.equals(2L)) {
-                    account = givenAccountEntityUser().orElseThrow();
-                    account.getPerson().setId(2L);
-                }
-                return Optional.ofNullable(account);
-            }).when(accountRepository).findById(anyLong());
-
-            lenient().doAnswer(invo -> {
                 FieldInfoError errorOptional = null;
                 if(invo.getArgument(0, Long.class).equals(2L)) {
                     errorOptional = new FieldInfoErrorBuilder().name("error").build();
                 }
                 return Optional.ofNullable(errorOptional);
-            }).when(identityVerificationService).validateUserAllowance(anyLong());
+            }).when(identityVerificationService).validateAllowanceByAccountId(anyLong());
 
         }
-
-        @Test
-        void shouldThrowExceptionWhenAccountIsNotFound() {
-            accountId = 3L;
-
-            String error = assertThatExceptionOfType(NotFoundException.class)
-                .isThrownBy(() -> workoutValidator.validateByIdRequest(accountId)).actual().getMessage();
-
-            assertThat(error).isEqualTo("Account not found");
-
-            verify(accountRepository, times(1)).findById(accountId);
-            verifyNoInteractions(identityVerificationService);
-        }
-
 
         @Test
         void shouldThrowValidationExceptionWhenUserIsNotAllowed() {
@@ -100,8 +67,7 @@ public class WorkoutValidatorUnitTest {
 
             assertThat(errorName).isEqualTo("error");
 
-            verify(accountRepository, times(1)).findById(accountId);
-            verify(identityVerificationService).validateUserAllowance(eq(2L));
+            verify(identityVerificationService).validateAllowanceByAccountId(accountId);
         }
 
         @Test
@@ -109,8 +75,7 @@ public class WorkoutValidatorUnitTest {
             accountId = 1L;
             assertDoesNotThrow(() -> workoutValidator.validateByIdRequest(accountId));
 
-            verify(accountRepository, times(1)).findById(accountId);
-            verify(identityVerificationService).validateUserAllowance(eq(1L));
+            verify(identityVerificationService).validateAllowanceByAccountId(accountId);
         }
 
     }
