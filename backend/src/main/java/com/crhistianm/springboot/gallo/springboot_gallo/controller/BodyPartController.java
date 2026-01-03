0504ce8f7a660f4203fbee2cb53bcd038a3233ca
CrhistianMRe <came.29meca@gmail.com>
2026-01-03 16:44:32 +0800

    fix(backend): replace BodyPartController stereotype annotation to match correct context

--- backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/controller/BodyPartController.java
+++ backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/controller/BodyPartController.java
@@ -3,14 +3,14 @@ package com.crhistianm.springboot.gallo.springboot_gallo.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.crhistianm.springboot.gallo.springboot_gallo.dto.BodyPartResponseDto;
 import com.crhistianm.springboot.gallo.springboot_gallo.service.BodyPartService;
 
-@Controller
+@RestController
 @RequestMapping("api/body-parts")
 public class BodyPartController {
 
