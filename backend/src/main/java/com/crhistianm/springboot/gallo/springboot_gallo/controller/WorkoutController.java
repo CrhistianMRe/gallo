74439dcff541091e24d999366b1015ff92409d5e
CrhistianMRe <came.29meca@gmail.com>
2026-01-03 16:47:43 +0800

    fix(backend): replace WorkoutController stereotype annotation to match correct context

--- backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/controller/WorkoutController.java
+++ backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/controller/WorkoutController.java
@@ -2,16 +2,16 @@ package com.crhistianm.springboot.gallo.springboot_gallo.controller;
 
 import org.springframework.data.web.PagedModel;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.crhistianm.springboot.gallo.springboot_gallo.dto.WorkoutResponseDto;
 import com.crhistianm.springboot.gallo.springboot_gallo.service.WorkoutService;
 
-@Controller
+@RestController
 @RequestMapping("api/workouts")
 public class WorkoutController {
 
