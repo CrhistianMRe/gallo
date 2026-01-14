a2928efb8d14661646ed3732143d943d84273526
CrhistianMRe <came.29meca@gmail.com>
2026-01-14 14:47:05 +0800

    test(backend): create feature-based exercise test module

---
+++ backend/src/test/java/com/crhistianm/springboot/gallo/springboot_gallo/exercise/ExerciseData.java
@@ -0,0 +1,7 @@
+package com.crhistianm.springboot.gallo.springboot_gallo.exercise;
+
+public class ExerciseData {
+
+    public static Exercise getExerciseInstance(){ return new Exercise(); }
+    
+}
