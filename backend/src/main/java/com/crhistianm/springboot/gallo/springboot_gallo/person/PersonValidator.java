cee189015ac9312f5c3b2f0e2337807e95cf1d14
CrhistianMRe <came.29meca@gmail.com>
2026-04-18 16:38:00 +0800

    refactor: add custom error list usage for service validators

--- backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/person/PersonValidator.java
+++ backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/person/PersonValidator.java
@@ -1,13 +1,8 @@
 package com.crhistianm.springboot.gallo.springboot_gallo.person;
 
-
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Component;
 
-import com.crhistianm.springboot.gallo.springboot_gallo.shared.exception.ValidationServiceException;
-import com.crhistianm.springboot.gallo.springboot_gallo.shared.FieldInfoError;
+import com.crhistianm.springboot.gallo.springboot_gallo.shared.ValidationServiceErrorList;
 import com.crhistianm.springboot.gallo.springboot_gallo.account.IdentityVerificationService;
 
 
@@ -24,11 +19,12 @@ class PersonValidator {
     }
 
     void validateRequest(Long pathPersonId, PersonRequestDto personDto) {
-        List<FieldInfoError> fields = new ArrayList<>();
+        ValidationServiceErrorList errorList = new ValidationServiceErrorList();
         //If is null it is basically a create not update request so this validation is not activated
-        identityService.validateUserAllowanceByPersonId(pathPersonId).ifPresent(fields::add);
-        personService.validateUniquePhoneNumber(pathPersonId, personDto).ifPresent(fields::add);
-        if(!fields.isEmpty()) throw new ValidationServiceException(fields);
+        identityService.validateUserAllowanceByPersonId(pathPersonId).ifPresent(errorList::add);
+        personService.validateUniquePhoneNumber(pathPersonId, personDto).ifPresent(errorList::add);
+
+        errorList.throwFieldErrors();
     }
 
 
