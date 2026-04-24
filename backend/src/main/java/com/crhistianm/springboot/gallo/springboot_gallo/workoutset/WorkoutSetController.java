cebfab642198410a68b40cf2efc87ed391a4642a
CrhistianMRe <came.29meca@gmail.com>
2026-04-24 09:58:38 +0800

    feat: add workout set view all by id functionality

--- backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/workoutset/WorkoutSetController.java
+++ backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/workoutset/WorkoutSetController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +28,12 @@ class WorkoutSetController {
     ResponseEntity<List<WorkoutSetResponseDto>> createAll(@Validated (GroupsOrder.class) @RequestBody WorkoutSetRequestDto requestDto) {
         List<WorkoutSetResponseDto> responseDto = workoutSetService.saveAll(requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    }
+
+    @GetMapping("/{workoutId}")
+    ResponseEntity<List<WorkoutSetResponseDto>> findAllByWorkoutId(@PathVariable Long workoutId) {
+        List<WorkoutSetResponseDto> responseList = workoutSetService.getAllByWorkoutId(workoutId);
+        return ResponseEntity.ok(responseList);
     }
 
 }
