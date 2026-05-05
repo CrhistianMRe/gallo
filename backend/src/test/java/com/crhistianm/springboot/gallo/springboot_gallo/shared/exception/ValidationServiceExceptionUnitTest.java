609058a1d4c72f873116e45c2ed588fdc873a1ba
CrhistianMRe <came.29meca@gmail.com>
2026-05-05 15:04:42 +0800

    test: rework validation service exception working tests

--- backend/src/test/java/com/crhistianm/springboot/gallo/springboot_gallo/shared/exception/ValidationServiceExceptionUnitTest.java
+++ backend/src/test/java/com/crhistianm/springboot/gallo/springboot_gallo/shared/exception/ValidationServiceExceptionUnitTest.java
@@ -1,58 +1,45 @@
 package com.crhistianm.springboot.gallo.springboot_gallo.shared.exception;
 
-import static com.crhistianm.springboot.gallo.springboot_gallo.shared.Data.givenFieldInfoErrorOne;
-import static com.crhistianm.springboot.gallo.springboot_gallo.shared.Data.givenFieldInfoErrorTwo;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import com.crhistianm.springboot.gallo.springboot_gallo.shared.FieldInfoError;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import com.crhistianm.springboot.gallo.springboot_gallo.shared.exception.ValidationServiceException;
 import org.junit.jupiter.api.Test;
 
+import com.crhistianm.springboot.gallo.springboot_gallo.shared.FieldInfoError;
+import com.crhistianm.springboot.gallo.springboot_gallo.shared.FieldInfoErrorBuilder;
 
 class ValidationServiceExceptionUnitTest {
 
-    ValidationServiceException validationException;
+    private ValidationServiceException validationException;
 
     @Test
-    void shouldLoadFieldInfoErrorListWhenIsPassedThroughConstructor() {
-        List<FieldInfoError> fields = new ArrayList<>();
+    void shouldReturnExceptionWithDefaultMessage() {
 
-        fields.add(givenFieldInfoErrorOne().orElseThrow());
-        fields.add(givenFieldInfoErrorTwo().orElseThrow());
+       validationException = new ValidationServiceException();
 
-
-        validationException = assertThatExceptionOfType(ValidationServiceException.class).isThrownBy(() ->{
-            throw new ValidationServiceException();
-        }).actual();
-
-        FieldInfoError field1 = fields.stream().filter(error -> error.getName().equals("test1")).findFirst().orElseThrow();
-        FieldInfoError field2 = fields.stream().filter(error -> error.getName().equals("test2")).findFirst().orElseThrow();
-        
-
-
-        assertThat(field1).extracting(FieldInfoError::getName).isEqualTo("test1");
-        assertThat(field1).extracting(FieldInfoError::getValue).isEqualTo("testvalue1");
-        assertThat(field1).extracting(FieldInfoError::getType).isEqualTo(String.class);
-        assertThat(field1).extracting(FieldInfoError::getOwnerClass).isEqualTo(String.class);
-        assertThat(field1).extracting(FieldInfoError::getErrorMessage).isEqualTo("test error message 1");
-
-        assertThat(field2).extracting(FieldInfoError::getName).isEqualTo("test2");
-        assertThat(field2).extracting(FieldInfoError::getValue).isEqualTo(2L);
-        assertThat(field2).extracting(FieldInfoError::getType).isEqualTo(Long.class);
-        assertThat(field2).extracting(FieldInfoError::getOwnerClass).isEqualTo(Long.class);
-        assertThat(field2).extracting(FieldInfoError::getErrorMessage).isEqualTo("test error message 2");
+       assertThat(validationException)
+           .extracting(ValidationServiceException::getMessage)
+           .isEqualTo("Validation failed");
     }
 
     @Test
-    void shouldLoadMethodSourceNameWhenPrefixIsPassedThroughConstructor() {
-        validationException = assertThatExceptionOfType(ValidationServiceException.class).isThrownBy(()-> {
-            throw new ValidationServiceException(null, "should");
-        }).actual();
-        assertThat(validationException.getMethodSourceName()).contains("shouldLoadMethodSourceNameWhenPrefixIsPassedThroughConstructor");
+    void shouldLoadErrorList() {
+        final String methodSourcePrefix = "should";
+        List<FieldInfoError> errorList = new ArrayList<>();
+
+        FieldInfoError errorOne = new FieldInfoErrorBuilder().name("errorOne").build();
+        FieldInfoError errorTwo = new FieldInfoErrorBuilder().name("errorTwo").build();
+
+        errorList.add(errorOne);
+        errorList.add(errorTwo);
+
+        validationException = new ValidationServiceException(errorList, methodSourcePrefix);
+
+        List<FieldInfoError> expectedList = validationException.getFieldErrors();
+
+        assertThat(expectedList).hasSize(2);
     }
-    
+
 }
