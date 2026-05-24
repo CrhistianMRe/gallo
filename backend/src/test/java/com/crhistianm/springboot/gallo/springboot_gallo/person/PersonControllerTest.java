00d4132879ae8fa76c8d15e3e19a768e34fd82a8
CrhistianMRe <came.29meca@gmail.com>
2026-05-24 18:22:27 +0800

    test: fix tests broken adding cache to person module

--- backend/src/test/java/com/crhistianm/springboot/gallo/springboot_gallo/person/PersonControllerTest.java
+++ backend/src/test/java/com/crhistianm/springboot/gallo/springboot_gallo/person/PersonControllerTest.java
@@ -10,11 +10,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.web.PagedModel;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -41,6 +43,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 //remove security filters as is not needed in these tests
 @AutoConfigureMockMvc(addFilters = false)
 class PersonControllerTest {
+
+    @MockitoBean
+    private CacheManager cacheManager;
 
     @Autowired
     private MockMvc mockMvc;
