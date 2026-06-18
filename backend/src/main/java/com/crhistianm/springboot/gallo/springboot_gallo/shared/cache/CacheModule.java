6b4967aeab80a91e9a97282af484f34edbd0f9a2
CrhistianMRe <came.29meca@gmail.com>
2026-06-18 22:14:02 +0800

    perf: add cache to account module endpoints

--- backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/shared/cache/CacheModule.java
+++ backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/shared/cache/CacheModule.java
@@ -2,4 +2,5 @@ package com.crhistianm.springboot.gallo.springboot_gallo.shared.cache;
 
 public class CacheModule {
     public static final String PERSON = "PERSON";
+    public static final String ACCOUNT = "ACCOUNT";
 }
