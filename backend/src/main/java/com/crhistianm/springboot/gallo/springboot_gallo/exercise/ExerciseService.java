439ba1d8d095cc1d2837305faa2cb70322d21935
CrhistianMRe <came.29meca@gmail.com>
2026-06-30 14:32:01 +0800

    perf: add exercise module cache

--- backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/exercise/ExerciseService.java
+++ backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/exercise/ExerciseService.java
@@ -1,8 +1,11 @@
 package com.crhistianm.springboot.gallo.springboot_gallo.exercise;
 
+import static com.crhistianm.springboot.gallo.springboot_gallo.shared.cache.CacheModule.EXERCISE;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +18,7 @@ class ExerciseService {
         this.exerciseRepository = exerciseRepository;
     }
 
+    @Cacheable(value = EXERCISE, key = "#root.methodName")
     @Transactional(readOnly = true)
     List<ExerciseResponseDto> getAll() {
         List<ExerciseResponseDto> responseList = exerciseRepository.findAll()
--- backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/shared/cache/CacheModule.java
+++ backend/src/main/java/com/crhistianm/springboot/gallo/springboot_gallo/shared/cache/CacheModule.java
@@ -3,4 +3,5 @@ package com.crhistianm.springboot.gallo.springboot_gallo.shared.cache;
 public class CacheModule {
     public static final String PERSON = "PERSON";
     public static final String ACCOUNT = "ACCOUNT";
+    public static final String EXERCISE = "EXERCISE";
 }
